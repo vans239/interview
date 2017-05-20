a4736562aa7798e50cac0e584d54bbe8519ec6d9
Evgeny Vanslov <vans239@gmail.com>
2017-05-20 13:44:57 +0400

    added codility task

---
+++ src/main/scala/codility/Equilibrium.scala
@@ -0,0 +1,25 @@
+package codility
+
+/**
+  * Created by KatKat on 20/05/17.
+  */
+import scala.collection.JavaConverters._
+
+//Find an index in an array such that its prefix sum equals its suffix sum.
+object Equilibrium {
+  def solution(a: Array[Int]): Int = {
+    val total = a.sum
+    var curr = 0
+    var i = 0
+    var found = false
+    while (i < a.length && !found) {
+      if (curr == total - curr - a(i)) {
+        found = true
+      }
+      curr += a(i)
+      i += 1
+    }
+    if (found) i - 1 else -1
+    // write your code in Scala 2.12
+  }
+}
